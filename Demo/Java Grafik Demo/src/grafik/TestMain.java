91a58c8b6f8198054ab19659479b9da96d5f425d
Tobias <pahlings@gmail.com>
2013-09-25 11:21:10 +0200

    Sprite Person ohne Schein hinzugefügt Fullscreen added

--- Demo/Java Grafik Demo/src/grafik/TestMain.java
+++ Demo/Java Grafik Demo/src/grafik/TestMain.java
@@ -3,7 +3,11 @@ package grafik;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 public class TestMain {
 	
@@ -24,16 +28,24 @@ public class TestMain {
 		painter = new PaintJob(fenster);
 		painter.start();
 		//TP optimierung  des Speicherverbrauchs erforderlich
+		BufferedImage sprite;
+		try {
+			sprite = ImageIO.read(new File("src\\grafik\\data\\demo_figur_ohneschein.png"));
 		
 		BufferedImage tempsprite = new  BufferedImage(90, 90, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2d = tempsprite.createGraphics();
-		g2d.setColor(new Color(1f,0f,0f,0.2f));
-		g2d.fillOval(0, 0, 45, 45);
+		g2d.setColor(new Color(1f,1f,1f,0.2f));
+		g2d.fillOval(2, 2, 41, 41);
 		g2d.fillOval(0, 45, 45, 45);
 		g2d.fillOval(45, 0, 45, 45);
 		g2d.fillOval(45, 45, 45, 45);
+		g2d.drawImage(sprite, 0, 0, null);		
 		entitys.add(new Person(200,200,tempsprite));
 		entitys.add(new Person(100,100,"src\\grafik\\data\\demo_person.png"));
 		entitys.get(0).start();
+		} catch (IOException e) {
+			System.err.println("Could not find sprite in location: "+"src\\grafik\\data\\demo_figur_ohneschein.png");
+			e.printStackTrace();
+		}
 	}
 }
--- Demo/Java Grafik Demo/src/grafik/Window.java
+++ Demo/Java Grafik Demo/src/grafik/Window.java
@@ -2,8 +2,10 @@ package grafik;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
@@ -13,11 +15,18 @@ import javax.swing.JFrame;
 
 public class Window extends JFrame{
 	private TestMain main;
+	private Dimension dim; //represents Game 0 Position
+	private Dimension screen; // represents the Screen
 	public Window(TestMain main){
 		this.main=main;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Zeichnen mit Java");
-        setSize(1125, 720);
+        screen = Toolkit.getDefaultToolkit().getScreenSize();
+        //setSize(1125, 720);
+        setSize((int)screen.getWidth(),(int)screen.getHeight());
+        dim = new Dimension();
+        dim.setSize((screen.getWidth()/2)-562,(screen.getHeight()/2)-360);
+        setBackground(Color.black);
         setUndecorated(true);
         setVisible(true);
         //TP Mouse Click Listener
@@ -28,9 +37,11 @@ public class Window extends JFrame{
 
 	public void paint(Graphics g){
 		//TP Double Frame Buffering to remove flickering
-		BufferedImage bufferedImage = new BufferedImage(1125, 720, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage bufferedImage = new BufferedImage((int)screen.getWidth(), (int)screen.getHeight(), BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2d = bufferedImage.createGraphics();
-		g2d.drawImage(main.karte.getImage(), 0, 0, 1125, 720, Color.black,null);
+		g2d.setColor(Color.black);
+		g2d.fillRect(0, 0,(int)screen.getWidth(), (int)screen.getHeight());
+		g2d.drawImage(main.karte.getImage(),(int)dim.getWidth(), (int)dim.getHeight(), 1125, 720, Color.black,null);
 	    for(int i=0;i<main.entitys.size();i++){
 	        g2d.drawImage(main.entitys.get(i).getGraphic(), main.entitys.get(i).getX(),main.entitys.get(i).getY(),main.entitys.get(i).width,main.entitys.get(i).height,null);
 	    }
@@ -42,15 +53,15 @@ public class Window extends JFrame{
 
 	    @Override
 	    public void mouseClicked(MouseEvent arg0) {
-	    	if((arg0.getX()/45)*45==1080 && (arg0.getY()/45)*45 == 0){
+	    	if((arg0.getX()-dim.getWidth()-1080)<45 && ((arg0.getY()-dim.getHeight())) < 45 && (arg0.getX()-dim.getWidth()-1080)>0 && ((arg0.getY()-dim.getHeight())) > 0){
 	    		
 	    		System.exit(0);
 	    		
 	    	}
-	        //System.out.println("Clicked: "+arg0.getX()+","+arg0.getY());
+	        System.out.println("Clicked: "+arg0.getX()+","+arg0.getY());
 	    	Person person = (Person)main.entitys.get(0);
-	    	person.setWpX((arg0.getX()/45)*45);
-	    	person.setWpY((arg0.getY()/45)*45);
+	    	person.setWpX(((arg0.getX()/45)*45)+((int)dim.getWidth()%45));
+	    	person.setWpY(((arg0.getY()/45)*45)+((int)dim.getHeight()%45));
 
 	    }
 
