186cd293c9dbf29744ae6f579f8223451cfacc36
Tobias <pahlings@gmail.com>
2013-10-23 22:37:58 +0200

    Nullpointer Exception from missing sprite removed in Agent & Kind
    Change-Id: Ie55a1e4a7f88bcb960b4c44ee9daf4146bd01f88
    Signed-off-by: Tobias <pahlings@gmail.com>

--- main/TheStreet/src/com/stalkindustries/main/game/Agent.java
+++ main/TheStreet/src/com/stalkindustries/main/game/Agent.java
@@ -4,5 +4,7 @@ import java.awt.image.BufferedImage;
 
 public class Agent extends Mensch {
 
-
+	public Agent(){
+		sprite=adults.getSubimage(0, 0, 90, 90);
+	}
 }
--- main/TheStreet/src/com/stalkindustries/main/game/Kinder.java
+++ main/TheStreet/src/com/stalkindustries/main/game/Kinder.java
@@ -1,12 +1,25 @@
 
 package com.stalkindustries.main.game;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public class Kinder extends Person {
 	
 	public Kinder(int house_id){
 		super(house_id);
-		//TODO: Kinder bauen
+		setSize(45, 45);
+		aussehen = new int[3];
+		aussehen[0]= (int)(Math.random()*4+1); //Körperbau
+		aussehen[1]= (int)(Math.random()*10+1); //Hautfarbe
+		aussehen[2]= (int)(Math.random()*10+1); //Frisur
+		sprite = new BufferedImage(90, 90, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2d = sprite.createGraphics();
+		g2d.drawImage(infants.getSubimage(90*aussehen[0], 0, 90, 90) ,0,0 , null); //Körperbau
+		g2d.drawImage(infants.getSubimage(90*aussehen[0]+360, (aussehen[1]-1)*90, 90, 90),0,0, null); //Hautfarbe
+		g2d.drawImage(infants.getSubimage(900, (aussehen[2]-1)*90, 90, 90),0,0, null); //Frisur
+		//TODO Geschlecht einbauen
+		//TODO arbeit oder nicht
+		step();
 	}
 }
