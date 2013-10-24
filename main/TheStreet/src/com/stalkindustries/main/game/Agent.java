e3b6551b98f8000059a6301bf77daebd88dc565e
Tobias <pahlings@gmail.com>
2013-10-24 20:04:45 +0200

    Agent wird jetzt gezeichnet
    Change-Id: I1ab1f41e0b45692b54b7e277fc4d773d1e8eb160
    Signed-off-by: Tobias <pahlings@gmail.com>

--- main/TheStreet/src/com/stalkindustries/main/game/Agent.java
+++ main/TheStreet/src/com/stalkindustries/main/game/Agent.java
@@ -5,6 +5,8 @@ import java.awt.image.BufferedImage;
 public class Agent extends Mensch { 
 
 	public Agent(){
+		setSize(Ressources.RASTERHEIGHT, Ressources.RASTERHEIGHT);
 		sprite=adults.getSubimage(0, 0, Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2);
+		step();
 	}
 }
