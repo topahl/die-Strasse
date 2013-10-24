3d4dcfe6a94d146991c0b82b9afce9102d38cbe7
Martika <Martika@MC2>
2013-10-24 13:43:31 +0200

    CSV_parser ist jetzt in Ressources

--- main/TheStreet/src/com/stalkindustries/main/game/Ressources.java
+++ main/TheStreet/src/com/stalkindustries/main/game/Ressources.java
@@ -1,5 +1,9 @@
 package com.stalkindustries.main.game;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import com.stalkindustries.main.TheStreet;
@@ -11,20 +15,46 @@ public class Ressources {
 	static{
 		//Initialisierung der location_ids
 		//TODO richtigen Dateipfad später angeben
-		location_ids=TheStreet.read_from_csv("C:/Users/Martika/Desktop/Dropbox/Software Engineering/Grafikdesign/Fertig/russland_map.csv");
+		location_ids=read_from_csv("C:/Users/Martika/Desktop/Dropbox/Software Engineering/Grafikdesign/Fertig/russland_map.csv");
 		
 		
 	}
 	
 	
 	
+	//Beschwerden an Sven und Miri
+		public static ArrayList<ArrayList<String>> read_from_csv(String dateiName){
+			ArrayList<ArrayList<String>> list_of_lists = new ArrayList<ArrayList<String>>();
+			
+			//File vorhanden?
+			File file = new File(dateiName);
+	        if (!file.canRead() || !file.isFile()){
+	        	System.out.print("No file found.");
+	            System.exit(0);
+	        }
+	        
+	        try {
+	            BufferedReader in = new BufferedReader(new FileReader(dateiName));
+	            String zeile = null;
+	            ArrayList<String> tmp;
+	            while ((zeile = in.readLine()) != null) {
+	            	tmp = new ArrayList<String>();
+	            	for(int i=0;i<zeile.split(";").length;i++){
+	            		tmp.add(zeile.split(";")[i]);	//Spalte hinzufügen
+	            	}
+	            	list_of_lists.add(tmp);	//Zeile hinzufügen
+	            }
+	            in.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } 
+			
+			return list_of_lists;
+		}
+	
+		
 	public static ArrayList<ArrayList<String>> getLocation_ids() {
 		return location_ids;
 	}
-
-
-//
-//	public void setLocation_ids(ArrayList<ArrayList<String>> location_ids) {
-//		this.location_ids = location_ids;
-//	}
+	
 }
