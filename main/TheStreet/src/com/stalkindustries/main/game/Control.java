a2c474fbcc68d8a2772e0b6bbc47c82dbe3b8380
Sven <Sven@Sven-Privat>
2013-10-25 13:38:27 +0200

    Erstellen der Clickmethoden der schon vorhandenen Buttons

--- main/TheStreet/src/com/stalkindustries/main/game/Control.java
+++ main/TheStreet/src/com/stalkindustries/main/game/Control.java
@@ -2,4 +2,56 @@ package com.stalkindustries.main.game;
 
 public class Control {
 
+	void Pauseclick(){
+		
+	}
+	
+	void Exitclick(){
+		
+	}
+	
+	void Spionageaktionclick(){
+		
+	}
+	
+	void Wanzenclick(){
+		
+	}
+	
+	void Kameraclick(){
+		
+	}
+	
+	void Hackenclick(){
+		
+	}
+	
+	void Fernglassclick(){
+		
+	}
+	
+	void Beschwichtigenaktionclick(){
+		
+	}
+	
+	void Kuchenclick(){
+		
+	}
+	
+	void Unterhaltenclick(){
+		
+	}
+	
+	void Flirtenclick(){
+		
+	}
+	
+	void Nachhauseclick(){
+		
+	}
+	
+	void Verhaftenclick(){
+		
+	}
+
 }
