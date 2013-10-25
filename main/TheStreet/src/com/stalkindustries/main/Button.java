468cbab4ce797c2d9aaa7110a0f2cbc070feebd8
Tobias <pahlings@gmail.com>
2013-10-25 20:27:42 +0200

    Button API created (untested)
    Change-Id: Iccda2d6365dfff7dd7ee2992cded4b371e76bd45
    Signed-off-by: Tobias <pahlings@gmail.com>

--- main/TheStreet/src/com/stalkindustries/main/Button.java
+++ main/TheStreet/src/com/stalkindustries/main/Button.java
@@ -1,5 +1,39 @@
 package com.stalkindustries.main;
 
-public class Button {
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
+import javax.swing.JButton;
+
+import com.stalkindustries.main.game.Control;
+
+public class Button extends JButton implements ActionListener{
+	
+	private String ok_code;
+	private Control controlunit;
+	
+	public Button(Control controlunit, BufferedImage normal,BufferedImage hover, BufferedImage clicked, String funktionsname, int x, int y){
+		this.ok_code = funktionsname;
+		addActionListener(this);
+		
+		setIcon(new javax.swing.ImageIcon(normal));
+		setRolloverIcon(new javax.swing.ImageIcon(hover));
+        setPressedIcon(new javax.swing.ImageIcon(clicked));
+        
+        setBorder(null);
+        setBorderPainted(false);
+        setContentAreaFilled(false);
+        setPreferredSize(new Dimension(normal.getWidth(),normal.getHeight()));
+        
+        setBounds(x, y, normal.getWidth(),normal.getHeight());
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		controlunit.call(ok_code);
+		
+	}
+	
 }
--- main/TheStreet/src/com/stalkindustries/main/game/Control.java
+++ main/TheStreet/src/com/stalkindustries/main/game/Control.java
@@ -3,7 +3,13 @@ package com.stalkindustries.main.game;
 import java.awt.event.ActionEvent;
 
 public class Control {
+	
+	public void call(String funktion){ //TODO implement
+		System.out.println("You pressed:"+funktion);
+	}
 
+	
+	
 	void Pauseclick(ActionEvent evt){
 		
 	}
