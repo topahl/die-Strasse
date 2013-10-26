39a5b7677cf51927a6378bed1f5d543571c62efc
Sven <Sven@Sven-Privat>
2013-10-26 17:53:09 +0200

    Funktion eingefügt, dass Kinder und Erwachsene auf wunsch farbig gezeichnet werden

--- main/TheStreet/src/com/stalkindustries/main/game/Erwachsene.java
+++ main/TheStreet/src/com/stalkindustries/main/game/Erwachsene.java
@@ -8,6 +8,7 @@ public class Erwachsene extends Person{
 	private boolean hat_arbeit;	
 	public Erwachsene(int house_id){
 		super(house_id);
+		this.haus_id=house_id;
 		
 		//hat Arbeit?
 		int tmp = (int)(Math.random()*3+1);
@@ -16,14 +17,20 @@ public class Erwachsene extends Person{
 		else
 			this.hat_arbeit = true;
 		
-		setSize(Ressources.RASTERHEIGHT, Ressources.RASTERHEIGHT);
+		setSize(Ressources.RASTERHEIGHT, Ressources.RASTERHEIGHT);		//Sven weis jetzt wie das funktioniert
 		aussehen = new int[3];
 		aussehen[0]= (int)(Math.random()*Ressources.TORSOCNT+1); //Körperbau
 		aussehen[1]= (int)(Math.random()*10+1); //Hautfarbe
 		aussehen[2]= (int)(Math.random()*10+1); //Frisur
+		
+		farbeZeigen(0);
+		}
+
+	private void farbeZeigen (int farbeZeigen){		//wenn farbeZeigen==1 dann wird Sie gezeigt sonst muss farbeZeigen ==0 sein. 
+													//Fragen an Sven
 		temp_sprite = new BufferedImage(Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2d = temp_sprite.createGraphics();
-		g2d.drawImage(adults.getSubimage(Ressources.RASTERHEIGHT*2*aussehen[0], (house_id+1)*Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2) ,0,0 , null); //Körperbau
+		g2d.drawImage(adults.getSubimage(Ressources.RASTERHEIGHT*2*aussehen[0], (this.haus_id+1)*Ressources.RASTERHEIGHT*2*farbeZeigen, Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2) ,0,0 , null); //Körperbau
 		g2d.drawImage(adults.getSubimage(Ressources.RASTERHEIGHT*2*aussehen[0]+Ressources.RASTERHEIGHT*Ressources.TORSOCNT*2, (aussehen[1]-1)*Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2),0,0, null); //Hautfarbe
 		g2d.drawImage(adults.getSubimage(Ressources.RASTERHEIGHT*Ressources.TORSOCNT*4+geschlecht*Ressources.RASTERHEIGHT*2, (aussehen[2]-1)*Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2),0,0, null); //Frisur
 		if(hat_arbeit)
@@ -32,8 +39,8 @@ public class Erwachsene extends Person{
 		update_schatten();
 		step();
 	}
-
-
+	
+	
 	public boolean isHat_arbeit() {
 		return hat_arbeit;
 	}
--- main/TheStreet/src/com/stalkindustries/main/game/Kinder.java
+++ main/TheStreet/src/com/stalkindustries/main/game/Kinder.java
@@ -8,19 +8,24 @@ public class Kinder extends Person {
 	
 	public Kinder(int house_id){
 		super(house_id);
-		setSize(Ressources.RASTERHEIGHT, Ressources.RASTERHEIGHT);
+		this.haus_id=house_id;
+		setSize(Ressources.RASTERHEIGHT, Ressources.RASTERHEIGHT);			//Sven weis jetzt wie das funktioniert
 		aussehen = new int[3];
 		aussehen[0]= (int)(Math.random()*Ressources.TORSOCNT+1); //Körperbau
 		aussehen[1]= (int)(Math.random()*10+1); //Hautfarbe
 		aussehen[2]= (int)(Math.random()*10+1); //Frisur
 		temp_sprite = new BufferedImage(Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2, BufferedImage.TYPE_INT_ARGB);
+
+		farbeZeigen(0);
+	}
+	private void farbeZeigen (int farbeZeigen){		//wenn farbeZeigen==1 dann wird Sie gezeigt sonst muss farbeZeigen ==0 sein. 
+													//Fragen an Sven
 		Graphics2D g2d = temp_sprite.createGraphics();
-		g2d.drawImage(infants.getSubimage(Ressources.RASTERHEIGHT*2*aussehen[0], (house_id+1)*Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2) ,0,0 , null); //Körperbau
+		g2d.drawImage(infants.getSubimage(Ressources.RASTERHEIGHT*2*aussehen[0], (this.haus_id+1)*Ressources.RASTERHEIGHT*2*farbeZeigen, Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2) ,0,0 , null); //Körperbau
 		g2d.drawImage(infants.getSubimage(Ressources.RASTERHEIGHT*2*aussehen[0]+360, (aussehen[1]-1)*Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2),0,0, null); //Hautfarbe
 		g2d.drawImage(infants.getSubimage(Ressources.RASTERHEIGHT*Ressources.TORSOCNT*4+geschlecht*Ressources.RASTERHEIGHT*2, (aussehen[2]-1)*Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2),0,0, null); //Frisur
-		//TODO Geschlecht einbauen
-		//TODO arbeit oder nicht
 		update_schatten();
 		step();
+			
 	}
 }
