c0b49012c19b9cc772eeef531839a347069d9e28
Martika <Martika@MC2>
2013-11-02 15:37:54 +0100

    Zeitverzögerungsbug (60) behoben
    Change-Id: Idf045e85537a6868d29cf8d0a9d316d1c3ed43df
    Signed-off-by: Martika <Martika@MC2>

--- main/TheStreet/src/com/stalkindustries/main/game/Person.java
+++ main/TheStreet/src/com/stalkindustries/main/game/Person.java
@@ -56,7 +56,7 @@ public abstract class Person extends Mensch {
 			this.bewegungsgeschwindigkeit = 3;
 		
 		this.geschlecht = (int)(Math.random()*(2)+1);
-		this.zeitverzogerung = (int)(Math.random()*(60))+1;
+		this.zeitverzogerung = (int)(Math.random()*(59))+1;
 		this.haus_id = house_id;
 				
 
