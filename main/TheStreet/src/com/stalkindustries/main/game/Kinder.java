518d00526ce11511ccba3d7d8d59299b6b41489c
Tobias <pahlings@gmail.com>
2013-11-09 17:10:14 +0100

    BUGFIX: Kinder pixeln nicht mehr
    Change-Id: I8d5e221f09af8588788b3ae1a38953bf6a943437
    Signed-off-by: Tobias <pahlings@gmail.com>

--- main/TheStreet/src/com/stalkindustries/main/game/Kinder.java
+++ main/TheStreet/src/com/stalkindustries/main/game/Kinder.java
@@ -21,6 +21,7 @@ public class Kinder extends Person {
 	}
 	public void farbeZeigen (boolean farbeZeigen){		//wenn farbeZeigen==1 dann wird Sie gezeigt sonst muss farbeZeigen ==0 sein. 
 													//Fragen an Sven
+		temp_sprite = new BufferedImage(Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2d = temp_sprite.createGraphics();
 		g2d.drawImage(infants.getSubimage(Ressources.RASTERHEIGHT*2*aussehen[0], (this.haus_id+1)*Ressources.RASTERHEIGHT*2*(farbeZeigen?1:0), Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2) ,0,0 , null); //Körperbau
 		g2d.drawImage(infants.getSubimage(Ressources.RASTERHEIGHT*2*aussehen[0]+360, (aussehen[1]-1)*Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2, Ressources.RASTERHEIGHT*2),0,0, null); //Hautfarbe
